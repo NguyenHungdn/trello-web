853dc6621c27343d2129efb2dd252a6c8245b4d8
nguyenhung <antialonedn1@gmail.com>
2023-08-29 16:24:59 +0700

    config mui theme

--- note.md
+++ note.md
@@ -2,3 +2,5 @@ version
 js vs jsx
 clean code
 eslintrc
+css_base_line
+MUI -> theming --> Default Theme viewer
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Button } from '@mui/material'
+import Typography from '@mui/material/Typography'
 function App() {
   return (
     <>
       <div> NguyenHung</div>
+      <Typography variant="body2" color="text.secondary">
+        test Typography
+      </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
