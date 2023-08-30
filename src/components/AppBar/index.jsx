70a4b9fdcb693fb825b7a3cd307ab2afd13cac93
nguyenhung <antialonedn1@gmail.com>
2023-08-31 01:47:44 +0700

    refactor

---
+++ src/components/AppBar/index.jsx
@@ -0,0 +1,23 @@
+import { Box } from '@mui/material'
+import ModeSelect from '../ModeSelect'
+
+function AppBar() {
+  return (
+    <div>
+      {' '}
+      <Box
+        sx={{
+          backgroundColor: 'primary.light',
+          width: '100%',
+          height: (theme) => theme.trello.appBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <ModeSelect />
+      </Box>
+    </div>
+  )
+}
+
+export default AppBar
